18b4af46432594a43095cb3f183636cf6309796c
James <jamescarclan@yahoo.com>
2020-06-25 21:53:39 +0200

    ChronometreTwoDigit

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,24 +1,53 @@
 class Chronometer {
   constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
     // ... your code goes here
   }
   startClick(callback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => this.currentTime++, 1000);
   }
   getMinutes() {
     // ... your code goes here
+    // currentTime = 69
+
+    return Math.floor(this.currentTime / 60);
   }
+
+  // getMinutes() = 1
   getSeconds() {
     // ... your code goes here
+    // currentTime = 69
+
+    return this.currentTime % 60;
   }
+  // getSeconds() = 9
+
   twoDigitsNumber() {
-    // ... your code goes here
+    //
+    let min = this.getMinutes();
+    // min = 1
+    let sec = this.getSeconds();
+    // sec = 9
+
+    if (min < 10) {
+      min = `0${min}`;
+    }
+
+    if (sec < 10) {
+      sec = `0${sec}`;
+    }
+
+    return `${min}:${sec}`;
   }
   stopClick() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
     // ... your code goes here
+    this.currentTime = 0;
   }
   splitClick() {
     // ... your code goes here
