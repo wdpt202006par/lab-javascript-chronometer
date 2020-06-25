303c07aac42a1f28766e4b37f76ea0eab9188a86
Lekmiti Wafaa <lekmiti.wafaa@gmail.com>
2020-06-25 20:38:07 +0200

    methode startClick done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,9 +1,14 @@
 class Chronometer {
+
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
+
   }
   startClick(callback) {
-    // ... your code goes here
+    setInterval(() => {
+      this.currentTime ++;
+    }, 1000)
   }
   getMinutes() {
     // ... your code goes here
