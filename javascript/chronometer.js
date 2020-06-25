2cbc0a58ffb628ec73993777af9fa04c71cc9b97
Laura Bonavent <laura.bonavent@gmail.com>
2020-06-25 20:27:41 +0200

    getMinutes

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,12 +1,17 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
+
   startClick(callback) {
-    // ... your code goes here
-  }
+    setInterval(() => {
+      this.currentTime++;
+    },1000)
+    };
+
   getMinutes() {
-    // ... your code goes here
+    return Math.trunc(this.currentTime/60);
   }
   getSeconds() {
     // ... your code goes here
